3ad85c597d6e2b85ecf835765f1bf58e92597c0f
Zac Westbrook <zacwestbrook24@gmail.com>
2026-05-18 22:55:41 -0500

    updated numpy practice (matrix multiplication)

--- numpyPrac.py
+++ numpyPrac.py
@@ -7,4 +7,20 @@ arr2 = np.array([[6, 7],
 
 arr_result = np.multiply(arr1, arr2)
 
-print(arr_result)
+print(f"Element-wise product of arr1 and arr2 is: \n{arr_result}")
+
+arr_result = np.matmul(arr1, arr2)
+
+print(f"Matrix product of arr1 and arr2 is: \n{arr_result}")
+
+arr_result = arr2 @ arr1
+
+print(f"Matrix product of arr2 and arr1 is: \n{arr_result}")
+
+arr_result = np.dot(arr1, arr2)
+
+print(f"Dot product of arr1 and arr2 is: \n{arr_result}")
+
+arr_result = np.dot(arr2, arr1)
+
+print(f"Dot product of arr2 and arr1 is: \n{arr_result}")
