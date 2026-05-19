61fb6324a19a16a9083754daa1e0ac3fb2a3e583
Zac Westbrook <zacwestbrook24@gmail.com>
2026-05-18 22:44:42 -0500

    initial commit: numpy matrix multiplication practice

---
+++ numpyPrac.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr1 = np.array([[2, 3],
+                 [4, 5]])
+arr2 = np.array([[6, 7],
+                 [3, 9]])
+
+arr_result = np.multiply(arr1, arr2)
+
+print(arr_result)
