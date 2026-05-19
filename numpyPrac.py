2c7c3dc6895136b41e4fd2f14d8f5dfeafda5b87
Zac Westbrook <zacwestbrook24@gmail.com>
2026-05-18 23:02:52 -0500

    matrix multiplication file

--- numpyPrac.py
+++ numpyPrac.py
@@ -5,6 +5,8 @@ arr1 = np.array([[2, 3],
 arr2 = np.array([[6, 7],
                  [3, 9]])
 
+vector = np.array([5, 7])
+
 arr_result = np.multiply(arr1, arr2)
 
 print(f"Element-wise product of arr1 and arr2 is: \n{arr_result}")
@@ -24,3 +26,11 @@ print(f"Dot product of arr1 and arr2 is: \n{arr_result}")
 arr_result = np.dot(arr2, arr1)
 
 print(f"Dot product of arr2 and arr1 is: \n{arr_result}")
+
+arr_result = np.matmul(arr1, vector.reshape(-1, 1))
+
+print(f"Matrix-vector product of arr1 and vector is: \n{arr_result}")
+
+arr_result = np.matmul(arr1, vector.reshape(-1, 1))
+
+print(f"Matrix-vector product of arr1 and vector (using reshape) is: \n{arr_result}")
